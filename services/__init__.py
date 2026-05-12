484d8d58d1ac57e5560f1a3f86f6135a23116b12
si21sio74kei-cmyk <si21sio74kei@gmail.com>
2026-05-12 21:05:57 +0800

    修复services模块导出

--- services/__init__.py
+++ services/__init__.py
@@ -1,6 +1,1 @@
 # Backend Services Module
-from .ai_engine import AISurvivalEngine
-from .survival_predictor import SurvivalPredictor
-from .energy_manager import EnergyManager
-from .food_manager import FoodManager
-from .emergency_protocol import EmergencyProtocol
